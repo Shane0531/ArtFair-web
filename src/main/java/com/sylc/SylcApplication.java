b5845d73248695c5801f79bae387ce62a2394364
SH <tjdghks0531@gmail.com>
2016-08-18 17:30:26 +0900

    first commit

---
+++ src/main/java/com/sylc/SylcApplication.java
@@ -0,0 +1,12 @@
+package com.sylc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SylcApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sylc/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.sylc.controller;
+
+import com.sylc.model.Employee;
+import com.sylc.repo.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+  @Autowired
+  EmployeeRepository em;
+
+  @RequestMapping("/api/employees")
+  public Iterable<Employee> findAllEmployee(){
+    return em.findAll();
+  }
+}
