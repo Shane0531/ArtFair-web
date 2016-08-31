30813c8e552d8583947b145e313898dd5e205e80
Shane <tjdghks0531@gmail.com>
2016-08-31 23:47:48 +0900

    cms - 배너 등록 추가

--- src/main/java/com/sylc/controller/api/cms/CmsBannerController.java
+++ src/main/java/com/sylc/controller/api/cms/CmsBannerController.java
@@ -5,6 +5,7 @@ import com.sylc.repository.BannerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,6 +23,11 @@ public class CmsBannerController {
     List<Banner> bannerList = banners.stream().sorted((o1, o2) -> (o1.getOrderNum() > o2.getOrderNum() ? 1 : -1)).collect(Collectors.toList());
     model.addAttribute("bannerList", bannerList);
     return "cms/banner/list";
+  }
 
+  @RequestMapping("/add")
+  public String addBanner(@ModelAttribute Banner banner) {
+    bannerRepo.save(banner);
+    return "redirect:/admin/banners/";
   }
 }
