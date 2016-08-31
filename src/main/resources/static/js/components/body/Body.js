19d56ef6b229c737bdd9092111ceda12ed267ec7
gustavolee <gglee89@gmail.com>
2016-08-31 19:14:37 +0900

    기본 Layout 설정

--- src/main/resources/static/js/components/body/Body.js
+++ src/main/resources/static/js/components/body/Body.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Slider from './Slider'
 
 class Body extends Component {
 
@@ -10,7 +9,7 @@ class Body extends Component {
           <h1 class="cover-heading">WELCOME</h1>
           <p class="lead">SYLC</p>
           <p class="lead">
-            <a href="#" class="btn btn-lg btn-default">ART FAIR 2016</a>
+            <a href="#" class="btn btn-lg btn-default call-to-action">ART FAIR 2016</a>
           </p>
         </div>
       </content>
--- src/main/resources/static/js/components/header/Header.js
+++ src/main/resources/static/js/components/header/Header.js
@@ -6,13 +6,48 @@ class Header extends Component {
       <header>
         <div class="masthead clearfix">
           <div class="inner">
-            <h3 class="masthead-brand">SYLC</h3>
-            <nav>
-              <ul class="nav masthead-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Features</a></li>
-                <li><a href="#">Contact</a></li>
-              </ul>
+            <nav class="navbar navbar-inverse navbar-static-top">
+              <div class="container">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">SYLC</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                  <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">About</a></li>
+                    <li><a href="#">Artists</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Location</a></li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right">
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">FAQ</a></li>
+                    <li class="nav-locale button-toolbar btn-toolbar" role="toolbar">
+                      <div class="button-group btn-group">
+                        <a class="link-locale" type="button" href="#">ENG</a>
+                        <a class="link-locale" type="button" href="#">KOR</a>
+                      </div>
+                    </li>
+                    <div class="hidden-sm hidden-md hidden-lg">
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Contact Us</a></li>
+                      <li><a href="#">FAQ</a></li>
+                      <li class="nav-locale button-toolbar btn-toolbar" role="toolbar">
+                        <div class="button-group btn-group">
+                          <a class="link-locale" type="button" href="#">ENG</a>
+                          <a class="link-locale" type="button" href="#">KOR</a>
+                        </div>
+                      </li>
+                    </div>
+                  </ul>
+                </div>
+              </div>
             </nav>
           </div>
         </div>
