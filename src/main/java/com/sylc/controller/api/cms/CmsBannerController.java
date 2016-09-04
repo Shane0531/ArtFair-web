cc051d58c96ed2f87b0369a5d9cd1769ce892cb1
Shane <tjdghks0531@gmail.com>
2016-09-04 19:25:42 +0900

    편집 기능 추가

--- src/main/java/com/sylc/controller/api/cms/CmsBannerController.java
+++ src/main/java/com/sylc/controller/api/cms/CmsBannerController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,7 +18,7 @@ public class CmsBannerController {
   BannerRepo bannerRepo;
 
   @RequestMapping("/")
-  public String listBanner(Model model) {
+  public String listBannerView(Model model) {
     List<Banner> banners = bannerRepo.findAll();
     List<Banner> bannerList = banners.stream().sorted((o1, o2) -> (o1.getOrderNum() > o2.getOrderNum() ? 1 : -1)).collect(Collectors.toList());
     model.addAttribute("bannerList", bannerList);
@@ -35,5 +36,24 @@ public class CmsBannerController {
   public String deleteBanner(@RequestParam int idx) {
     bannerRepo.delete(idx);
     return "{\"msg\":\"OK\"}";
+  }
+
+  @RequestMapping("/{idx}")
+  public String editBannerView(@PathVariable int idx, Model model) {
+    Banner banner = bannerRepo.findOne(idx);
+    model.addAttribute("item", banner);
+    return "cms/banner/edit";
+  }
+
+  @RequestMapping(value = "/edit", method = RequestMethod.POST)
+  @Transactional
+  public String editBanner(@ModelAttribute Banner item) {
+    Banner banner = bannerRepo.findOne(item.getIdx());
+    banner.setName(item.getName());
+    banner.setWebImageUrl(item.getWebImageUrl());
+    banner.setMobileImageUrl(item.getMobileImageUrl());
+    banner.setOrderNum(item.getOrderNum());
+
+    return "redirect:/admin/banners/";
   }
 }
