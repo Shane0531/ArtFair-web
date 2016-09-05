d988fb65918b4a7c88e2cc61f112f8c82b9dd24b
SH <tjdghks0531@gmail.com>
2016-09-05 18:58:15 +0900

    event controller 추가

--- src/main/java/com/sylc/controller/restapi/EventController.java
+++ src/main/java/com/sylc/controller/restapi/EventController.java
@@ -1,10 +1,22 @@
 package com.sylc.controller.restapi;
 
+import com.sylc.model.event.Event;
+import com.sylc.repository.EventRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
 public class EventController {
+  @Autowired
+  EventRepo eventRepo;
+
+  @RequestMapping("/")
+  public List<Event> findAllEvent() {
+    return eventRepo.findAll();
+  }
 
 }
