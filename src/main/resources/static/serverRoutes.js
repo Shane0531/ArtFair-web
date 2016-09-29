5afbe496166b533389f8b8fe4750fcc20028b2c2
SH <tjdghks0531@gmail.com>
2016-09-29 18:50:39 +0900

    about페이지 추가

--- src/main/resources/static/serverRoutes.js
+++ src/main/resources/static/serverRoutes.js
@@ -3,6 +3,7 @@ import { Router, Route, IndexRoute } from 'react-router'
 import Contents from './js/components/content/Content'
 import Artists from './js/components/artist/Artist'
 import Events from './js/components/event/Event'
+import About from './js/components/about/About'
 import Index from './js/components/index/Slider'
 import Location from './js/components/location/Location'
 import App from './js/components/App'
@@ -13,6 +14,7 @@ const routes  = (
         <Route path="/artists" component={Artists}/>
         <Route path="/location" component={Location}/>
         <Route path="/events" component={Events}/>
+        <Route path="/about" component={About}/>
       </Route>
 )
 
