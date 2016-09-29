2ccb80f9b27179813caa401cbc3ebabace98eaf2
SH <tjdghks0531@gmail.com>
2016-09-29 14:33:04 +0900

    file serving complete

--- README.md
+++ README.md
@@ -9,3 +9,22 @@ resources/static
 
 npm run-script watch -> dev
 npm run-script build -> production
+
+
+
+Server Setup
+------------
+
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+sudo update-alternatives --config java
+apt-get install mysql-server-5.6
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+
+
+Deploy
+------
+
+cap production deploy:java
+
--- src/main/java/com/sylc/common/config/WebConfig.java
+++ src/main/java/com/sylc/common/config/WebConfig.java
@@ -1,13 +1,22 @@
 package com.sylc.common.config;
 
 import com.sylc.common.interceptor.LoginCheckInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+  @Value("${path.upload_dir:}")
+  String uploadDir;
+
+  @Value("${path.direct-serving:false}")
+  boolean isDirectServing;
+
+
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
@@ -17,4 +26,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     registry.addInterceptor(new LoginCheckInterceptor()).addPathPatterns("/admin/**/*").excludePathPatterns("/admin/login");
 
   }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    if (isDirectServing && uploadDir != null && uploadDir.length() > 0)
+      registry.addResourceHandler("/files/**").addResourceLocations("file:" + uploadDir);
+    super.addResourceHandlers(registry);
+  }
+
 }
