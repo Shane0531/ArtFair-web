aa31f1870f530fb4e8b10f2ed4ace6b3b1f50dad
Shane <tjdghks0531@gmail.com>
2016-10-09 21:38:26 +0900

    artist, event

--- src/main/java/com/sylc/controller/api/ArtistController.java
+++ src/main/java/com/sylc/controller/api/ArtistController.java
@@ -1,4 +1,14 @@
 package com.sylc.controller.api;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ArtistController {
+
+  @GetMapping("/artists")
+  public String viewArtist(){
+    return "app/artist/view";
+  }
+
 }
